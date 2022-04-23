373577224c4f3217637ce940072917f291157e83
John <vannok@live.com>
2022-04-23 15:02:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import Modall from "./Modall";
 
 const App = () => {
   const [openModal, setOpenModal] = useState(false);
-  const data = {
-    date: "",
-    duration: "",
-    grown: "",
-    children: "",
-    tel: "",
-    name: "",
-    diff: false,
-    mail: false,
-  };
-  console.log(data);
+
   return (
     <div>
       <button onClick={() => setOpenModal(!openModal)}>Open Modal</button>
       {openModal && (
-        <Modall data={data} openModal={openModal} setOpenModal={setOpenModal} />
+        <Modall openModal={openModal} setOpenModal={setOpenModal} />
       )}
     </div>
   );
