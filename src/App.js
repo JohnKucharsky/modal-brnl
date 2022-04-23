ad34c2667b7343d7972589c0de0bbda3347f203a
John <vannok@live.com>
2022-04-23 16:18:58 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Modall from "./Modall";
+import "./modal.scss";
 
 const App = () => {
   const [openModal, setOpenModal] = useState(false);
 
   return (
-    <div>
+    <div className="app">
       <button onClick={() => setOpenModal(!openModal)}>Open Modal</button>
       {openModal && (
         <Modall openModal={openModal} setOpenModal={setOpenModal} />
